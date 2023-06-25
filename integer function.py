7937ebae7bf65edde5724fdfa4a71fb093173b27
k4MAU0CODES <114183548+k4MAU0CODES@users.noreply.github.com>
2023-06-25 16:19:51 +0300

    Add files via upload

---
+++ integer function.py
@@ -0,0 +1,11 @@
+#retrieve even numbers from a given list
+def get_even_numbers(numbers):
+    even_numbers=[]
+    for num in numbers:
+        if num % 2 ==0:
+            even_numbers.append(num)
+    return even_numbers
+#input the number_list
+numbers =[1,2,3,4,5,6,7,8,9,12,10]
+even_numbers=get_even_numbers(numbers)
+print(even_numbers)
